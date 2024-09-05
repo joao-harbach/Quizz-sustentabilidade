5461a368e14db196126f948fd69501c020bf9c31
joao-harbach <132064275+joao-harbach@users.noreply.github.com>
2024-09-05 09:24:49 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('contactButton');
+    
+    button.addEventListener('click', () => {
+        alert('Obrigado por entrar em contato!');
+    });
+});
